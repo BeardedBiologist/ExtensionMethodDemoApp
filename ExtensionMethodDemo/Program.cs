e0aaa77f6213d92be831446f4b6f923b46c2ca6c
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-03 11:27:03 +0200

    init commit. Setup project.

--- ExtensionMethodDemo/Program.cs
+++ ExtensionMethodDemo/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace ExtensionMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
